9a690b1236328632b98b09b163663a1ed8a53d3e
lincoln <105800554+RaymondAhuren@users.noreply.github.com>
2025-08-15 20:57:54 +0000

    Update LoginForm.jsx

--- src/components/auth/LoginForm.jsx
+++ src/components/auth/LoginForm.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { NavLink, useNavigate } from "react-router";
 import { FiEye, FiEyeOff } from "react-icons/fi";
-import { useAuth } from "../../context/AuthContext";
+import { useAuth } from "../../context/authContext";
 
 const LoginForm = () => {
   const navigate = useNavigate();
